248ae85650dd05755499b03594cfbd46cd195090
yukimura51b <yukimura.3720@gmail.com>
2024-07-31 14:48:42 +0900

    gem minimagicを使って動的画像を生成する

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  
+  get 'images/ogp.png', to: 'images#ogp', as: 'images_ogp'
+
 
   # Defines the root path route ("/")
   # root "posts#index"
