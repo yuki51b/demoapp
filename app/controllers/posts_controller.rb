dc109dc0610ddd60f233d0b08d97441cb35e284b
yukimura51b <yukimura.3720@gmail.com>
2024-10-30 16:14:00 +0900

    test_teacher

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_30_165129) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_30_061926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
